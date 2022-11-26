193f1d0c13d2b04bdc6f3ae955da136e9d2b1bee
johnnylayson543 <B00148227@mytudublin.ie>
2022-11-26 13:40:44 +0000

    Updated PlayerController.cs and Spawn Manager
    - Added setBoundary() method and code to create boundary limit for the Player in the map
    - Adjusted Enemy Spawn Rate and Spawn Range
    - Adjusted Player speed and turn speed

--- IMM Project (Final) - Space Shooter/Assets/Scripts/PlayerController.cs
+++ IMM Project (Final) - Space Shooter/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     // Player Movement Variables and Properties
     private Rigidbody playerRb; // The Rigidbody variable of the Player
-    private float speed = 80.0f; // The speed of the Player
-    private float turnSpeed = 50.0f; // The turning speed of the player
+    private float speed = 100.0f; // The speed of the Player
+    private float turnSpeed = 80.0f; // The turning speed of the player
     private float horizontalInput;  // Horizontal Input Movements
     private float forwardInput;  // Vertical/Forward Input Movements
+    
+    // Player's Map Boundary Properties
+    private float xRange = 105; // The Map Boundary range of the Player in the map (x-axis)
+    private float zRange = 70; // The Map Boundary range of the Player in the map (z-axis)
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +33,32 @@ public class PlayerController : MonoBehaviour
         // Movement and Force of the Player
         playerRb.AddRelativeForce(Vector3.forward * Time.deltaTime * speed * forwardInput, ForceMode.Impulse); // Add a relative movement force for the Player
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput); // Add a rotation for the Player
+
+        setBoundaries(); // Set the Player Boundary
+    }
+
+    // Method to set the Player Boundary
+    public void setBoundaries()
+    {
+        // Boundaries
+        if (transform.position.x < -xRange)  // If player x position is less than -xRange (left)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); // Transform, stay and keep the player position in the left boundary
+        }
+
+        if (transform.position.x > xRange)  // If player x position is greater than xRange (right)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z); // Transform, stay and keep the player position in the right boundary
+        }
+
+        if (transform.position.z < -zRange)  // If player z position is less than -zRange (bottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange); // Transform, stay and keep the player position in the bottom boundary
+        }
+
+        if (transform.position.z > zRange)  // If player z position is greater than zRange (top)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange); // Transform, stay and keep the player position in the top boundary
+        }
     }
 }
