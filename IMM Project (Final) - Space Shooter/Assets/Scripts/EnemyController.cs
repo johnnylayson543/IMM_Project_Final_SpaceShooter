67e8ce1e6bbb020df1fc23d4c2694877effb0ea9
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2022-11-27 09:47:38 +0000

    update

--- IMM Project (Final) - Space Shooter/Assets/Scripts/EnemyController.cs
+++ IMM Project (Final) - Space Shooter/Assets/Scripts/EnemyController.cs
@@ -34,12 +34,21 @@ public class EnemyController : MonoBehaviour
         Target[] existingTargets = allTargets.Where(x => x != null).ToArray();
         Target[] physicalTargets = existingTargets.Where(x => x.getType() == Target.Type.Physical).ToArray();
 
-        // count the physical objects that exist
+        // count the physical yargets that exist
         int physicalTargetsLeft =  physicalTargets != null ? physicalTargets.Length : 0;
 
         // deduction: if no physical targets, go to the orgin; if there is one physical target, go to it; if there are two, go to nearest or farthest depending on the objective
-        Target closest = (physicalTargetsLeft == 0) ? originTarget : (physicalTargetsLeft == 1 ? physicalTargets[0] : (physicalTargets[0].getDistance() > physicalTargets[1].getDistance() ? physicalTargets[1] : physicalTargets[0]));
-        Target farthest = (physicalTargetsLeft == 0) ? originTarget : (physicalTargetsLeft == 1 ? physicalTargets[0] : (physicalTargets[0].getDistance() < physicalTargets[1].getDistance() ? physicalTargets[1] : physicalTargets[0]));
+        Target closest =  ((physicalTargetsLeft == 0) ? originTarget : (physicalTargetsLeft == 1 ? physicalTargets[0] : ((physicalTargetsLeft == 2) ? (physicalTargets[0].getDistance() > physicalTargets[1].getDistance() ? physicalTargets[1] : physicalTargets[0]): null)));
+        Target farthest = ((physicalTargetsLeft == 0) ? originTarget : (physicalTargetsLeft == 1 ? physicalTargets[0] : ((physicalTargetsLeft == 2) ? (physicalTargets[0].getDistance() < physicalTargets[1].getDistance() ? physicalTargets[1] : physicalTargets[0]): null)));
+
+        // if more than 2 physical targets, find min or max distance and find the respective array indices,
+        //   then get those items from physical targets category array
+        //   LINQ is used to make Lambda queries in LINQ array methods Min and Max
+        if (physicalTargetsLeft > 2 && (closest == null && farthest == null))
+        {
+            closest = physicalTargets[ Array.IndexOf(physicalTargets, physicalTargets.Min(x => x.getDistance() )) ];
+            farthest = physicalTargets[ Array.IndexOf(physicalTargets, physicalTargets.Max(x => x.getDistance() )) ];
+        }
         
         // if the target is far away, set a higher speed multipler based on the log10 of power of 1.3 of the distance;
         float speedMultiplier = (float) Math.Log10(Math.Pow(closest.getDistance(),1.3f));
