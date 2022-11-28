65f62e4e46bbebb2e19b763ab5fa2dfc582dd21b
johnnylayson543 <B00148227@mytudublin.ie>
2022-11-28 09:54:05 +0000

    Added SpaceShooter3
    - Added "SpaceShooter3" scene which adds the buttons for the difficulty levels (Easy, Medium, Hard) (For now since their script is not there yet, it does nothing but sends you to a generic gameplay)
    - Modified GameManager Script to added the GoToSpaceShooter3() method.

--- IMM Project (Final) - Space Shooter/Assets/Scripts/GameManager.cs
+++ IMM Project (Final) - Space Shooter/Assets/Scripts/GameManager.cs
@@ -49,6 +49,11 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene("SpaceShooter2"); // Go to the controls menu
     }
 
+    public void GoToSpaceShooter3()
+    {
+        SceneManager.LoadScene("SpaceShooter3"); // Go to the controls menu
+    }
+
     public void loadURL()
     {
         Application.OpenURL(URL); // Open the URL which contains the Game Files and Assets from the GitHub Repo
