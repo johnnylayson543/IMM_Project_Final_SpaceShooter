0d33e49bfccd1bc6072386f63cf5f7731190ebf3
johnnylayson543 <B00148227@mytudublin.ie>
2022-12-11 16:09:52 +0000

    Edited GameManager for score
    Set initial state of score to 0 on the GM (still buggy as there is a NullReferenceException when you start the game)

--- IMM Project (Final) - Space Shooter/Assets/Scripts/GameManager.cs
+++ IMM Project (Final) - Space Shooter/Assets/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScore(0); // Set the initial value of the score to 0
     }
 
     // Update is called once per frame
